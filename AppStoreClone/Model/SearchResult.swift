563202d5c8d439db0997f1eba4b61915e404b97a
ana-ge <ge.anatoliy@gmail.com>
2023-04-09 21:33:39 +0900

    Added SearchResult

---
+++ AppStoreClone/Model/SearchResult.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct SearchResult: Decodable {
+    let resultCount: Int
+    let results: [Result]
+}
+
+struct Result: Decodable {
+    let trackName: String?
+    let primaryGenreName: String?
+    let averageUserRating: Float?
+    let screenshotUrls: [String]
+    let artworkUrl100: String //app icon
+}
