9905e2ab12c3f6ccb2b134150841bb12151a5bc9
ana-ge <ge.anatoliy@gmail.com>
2023-04-09 21:25:03 +0900

    Added Service where we parsing JSON data

---
+++ AppStoreClone/Service/Service.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+class Service {
+    
+    static let shared = Service()
+    
+    func fetchApps(searchTerm: String, completion: @escaping ([Result], Error?) -> ()) {
+        let urlString =  "https://itunes.apple.com/search?term=\(searchTerm)&entity=software"
+
+        guard let url = URL(string: urlString) else { return }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Failed to fetch apps:", error)
+                completion([], nil)
+                return
+            }
+
+            guard let data = data else { return }
+
+            do {
+                let searchResult = try JSONDecoder().decode(SearchResult.self, from: data)
+                
+                completion(searchResult.results, nil)
+                
+            } catch let jsonError {
+                print("Failed to decode json", jsonError)
+                completion([], jsonError)
+            }
+        }.resume()
+    }
+}
