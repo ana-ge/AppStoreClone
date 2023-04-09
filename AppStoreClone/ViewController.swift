79d1353f50ac9037734c1584ad2fe940234b198f
ana-ge <ge.anatoliy@gmail.com>
2023-04-09 21:27:24 +0900

    Refactored ViewController

--- AppStoreClone/ViewController.swift
+++ AppStoreClone/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  AppStoreClone
-//
-//  Created by Анатолий Ге on 04.04.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
