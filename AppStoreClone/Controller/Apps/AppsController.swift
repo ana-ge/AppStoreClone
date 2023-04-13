32413174e1904fdba684912fd98f2aef5335e70c
ana-ge <ge.anatoliy@gmail.com>
2023-04-13 23:45:53 +0900

    Refactored AppsController

--- AppStoreClone/Controller/Apps/AppsController.swift
+++ AppStoreClone/Controller/Apps/AppsController.swift
@@ -6,7 +6,6 @@ class AppsController: BaseListController, UICollectionViewDelegateFlowLayout {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .yellow
         
         collectionView.register(AppsGroupCell.self, forCellWithReuseIdentifier: cellId)
     }
@@ -21,6 +20,10 @@ class AppsController: BaseListController, UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: view.frame.width, height: 250)
+        return .init(width: view.frame.width, height: 300)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 16, left: 0, bottom: 0, right: 0)
     }
 }
