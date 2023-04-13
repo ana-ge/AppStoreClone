dccd8c92c14c7e5cd0273f8685999e4d1da22c44
ana-ge <ge.anatoliy@gmail.com>
2023-04-13 23:44:37 +0900

    Refactored AppsGroupCell

--- AppStoreClone/Views/Apps/AppsGroupCell.swift
+++ AppStoreClone/Views/Apps/AppsGroupCell.swift
@@ -2,20 +2,18 @@ import UIKit
 
 class AppsGroupCell: UICollectionViewCell {
     
-    let titleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "App Section"
-        label.font = .boldSystemFont(ofSize: 30)
-        return label
-    }()
+    let titleLabel = UILabel(text: "App Section", font: .boldSystemFont(ofSize: 30))
     
+    let horizontalController = AppsHorizontalController()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .lightGray
-        
         addSubview(titleLabel)
-        titleLabel.fillSuperview()
+        titleLabel.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 0))
+        
+        addSubview(horizontalController.view)
+        horizontalController.view.anchor(top: titleLabel.bottomAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
     }
     
     required init?(coder: NSCoder) {
