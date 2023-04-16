dae087ba41163f97067ace8164302c7637d08cab
ana-ge <ge.anatoliy@gmail.com>
2023-04-16 22:19:25 +0900

    Added AppGroup

---
+++ AppStoreClone/Model/AppGroup.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct AppGroup: Decodable {
+    let feed: Feed
+}
+
+struct Feed: Decodable {
+    let title: String
+    let results: [FeedResult]
+}
+
+struct FeedResult: Decodable {
+    let name, artistName, artworkUrl100: String
+}
