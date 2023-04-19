317042c0beb13ebdd5e510644d54552e29a1c174
ana-ge <ge.anatoliy@gmail.com>
2023-04-19 22:53:50 +0900

    Refactored AppGroup

--- AppStoreClone/Model/AppGroup.swift
+++ AppStoreClone/Model/AppGroup.swift
@@ -4,10 +4,12 @@ struct AppGroup: Decodable {
     let feed: Feed
 }
 
+
 struct Feed: Decodable {
     let title: String
     let results: [FeedResult]
 }
+
 
 struct FeedResult: Decodable {
     let name, artistName, artworkUrl100: String
