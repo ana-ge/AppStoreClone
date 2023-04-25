12502910dabc9c50855beed98c5d7ce0f017d75f
ana-ge <ge.anatoliy@gmail.com>
2023-04-25 18:32:57 +0900

    Refactored Service

--- AppStoreClone/Service/Service.swift
+++ AppStoreClone/Service/Service.swift
@@ -4,74 +4,43 @@ class Service {
     
     static let shared = Service()
     
-    func fetchApps(searchTerm: String, completion: @escaping ([Result], Error?) -> ()) {
+    func fetchApps(searchTerm: String, completion: @escaping (SearchResult?, Error?) -> ()) {
         let urlString =  "https://itunes.apple.com/search?term=\(searchTerm)&entity=software"
-
-        guard let url = URL(string: urlString) else { return }
-
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print("Failed to fetch apps:", error)
-                completion([], nil)
-                return
-            }
-
-            guard let data = data else { return }
-
-            do {
-                let searchResult = try JSONDecoder().decode(SearchResult.self, from: data)
-                
-                completion(searchResult.results, nil)
-                
-            } catch let jsonError {
-                print("Failed to decode json", jsonError)
-                completion([], jsonError)
-            }
-        }.resume()
+        
+        fetchGenericJSONData(urlString: urlString, completion: completion)
     }
     
     
     func fetchTopGrossing(completion: @escaping (AppGroup?, Error?) -> ()) {
-        let urlString = "https://rss.itunes.apple.com/api/v1/us/ios-apps/new-games-we-love/all/50/explicit.json"
+        let urlString = "https://rss.itunes.apple.com/api/v1/us/ios-apps/top-grossing/all/50/explicit.json"
         fetchAppGroup(urlString: urlString, completion: completion)
     }
     
     func fetchGames(completion: @escaping (AppGroup?, Error?) -> ()) {
-        fetchAppGroup(urlString: "https://rss.itunes.apple.com/api/v1/us/ios-apps/new-games-we-love/all/50/explicit.json", completion: completion)
+        fetchAppGroup(urlString: "https://rss.itunes.apple.com/api/v1/us/ios-apps/new-games-we-love/all/50/explicit.json",
+            completion: completion)
     }
     
     func fetchAppGroup(urlString: String, completion: @escaping (AppGroup?, Error?) -> Void) {
-        guard let url = URL(string: urlString) else { return }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            
-            if let error = error {
-                completion(nil, error)
-                return
-            }
-    
-            do {
-                let appGroup = try JSONDecoder().decode(AppGroup.self, from: data!)
-                completion(appGroup, nil)
-            } catch {
-                completion(nil, error)
-            }
-        }.resume()
+        fetchGenericJSONData(urlString: urlString, completion: completion)
     }
     
     func fetchSocialApps(completion: @escaping ([SocialApp]?, Error?) -> Void) {
-        let urlString = "https://api.letsbuildthatapp.com/appstore.social"
+        let urlString = "https://api.letsbuildthatapp.com/appstore/social"
+        fetchGenericJSONData(urlString: urlString, completion: completion)
+    }
+    
+    func fetchGenericJSONData<T: Decodable>(urlString: String, completion: @escaping (T?, Error?) -> ()) {
+        let urlString = "https://api.letsbuildthatapp.com/appstore/social"
         guard let url = URL(string: urlString) else { return }
-        
         URLSession.shared.dataTask(with: url) { data, response, error in
-            
             if let error = error {
                 completion(nil, error)
                 return
             }
-    
             do {
-                let objects = try JSONDecoder().decode([SocialApp].self, from: data!)
+                
+                let objects = try JSONDecoder().decode(T.self, from: data!)
                 completion(objects, nil)
             } catch {
                 completion(nil, error)
@@ -79,3 +48,18 @@ class Service {
         }.resume()
     }
 }
+
+class Stack<T: Decodable> {
+    var items = [T]()
+    func push(item: T) { items.append(item)}
+    func pop() -> T? {return items.last}
+}
+
+func stackFunc() {
+    let stackOfStrings = Stack<String>()
+    stackOfStrings.push(item: "This has to be a string")
+    
+    let stackOfInts = Stack<Int>()
+    stackOfInts.push(item: 1)
+}
+
